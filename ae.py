cdec771e174589154b3e7e1636880e2766ba5f46
BlaiseCz <blazej.czekala@gmail.com>
2022-06-11 00:52:45 +0200

    ae started

--- ae.py
+++ ae.py
@@ -1,21 +1,42 @@
+from world import World
 from random import random
 
-from parser import Object
+def init_pop(pop_size=100) -> list(World):
+    population = list()
+    for id in range(pop_size):
+        world = World(id)
+        world.read_from_xml('./geneticquest_db.xml')
+        population.append(world)
+        print(f"World #{world.id} created")
+
+    return population
+
+def mutate_member(member, p=0.1):
+    if p < random():
+        member.add_random_goal()
+    if p < random():
+        member.remove_random_goal()
+
+    return member
 
 
-def add_random_predicate(ind):
+def calc_fitness(sollution):
     pass
 
-def remove_random_predicat(ind):
-    pass
 
-def mutate_pop(pop, p=.05) -> list[Object]:
+def assign_fitness(pop):
+    # load sollutions.all file, then assign proper fitness
+    # load file
+    loaded_file = [] #TODO
     for individual in pop:
-        if random() < p:
-            remove_random_predicat(individual.predicate)
-        if random() < p:
-            add_random_predicate(individual.predicate)
-        #TODO
-        pass
+        individual.calculate_fitness(loaded_file[individual.id])
 
-    return pop
+    pass
+
+
+def remove_worst_from_pop(last):
+    pass
+
+
+def add_new_members(pop):
+    pass
--- runner.py
+++ runner.py
@@ -1,7 +1,10 @@
 import os
 
-HSPHOME = os.path.join("hsp-planners/hsp-1.12")
+from ae import init_pop, mutate_member, remove_worst_from_pop, add_new_members, assign_fitness
 
+HSPHOME = os.path.join("hsp-planners/hsp-1.12")
+POP_SIZE = 100
+ITERATIONS = 140
 
 def run_hsp_planner(problem_path: str) -> None:
     dir_path = os.path.abspath(HSPHOME)
@@ -14,19 +17,25 @@ def run_hsp_planner(problem_path: str) -> None:
               f"make compile;"
               f"make solve;")
 
+
 """
     ae loop -> runs population ae algorithm in loop with mutation
 """
+
+
 if __name__ == "__main__":
     # TEST
-    run_hsp_planner(f'/pddl/story')
+    # run_hsp_planner(f'/pddl/story')
+    pop = init_pop(POP_SIZE)
 
-    # TODO
-    # pop = init_pop(100)
+    for _ in range(ITERATIONS):
+        for member in pop():
+            mut = mutate_member(member)
+            mut.save_to_pddl()
+            save_path = dir_path = os.path.abspath(HSPHOME) + '/pddl/story/'
+            mut.save_to_pddl(save_path + 'domain' + mut.id + '.pddl', save_path + 'problem' + mut.id + '.pddl')
 
-    # for member in pop():
-    #     mutate_pop(pop)
-    #     run_hsp_planner()
-    #     calc_fitness(pop)
-    #     remove_worst_from_pop(last=10)
-    #     add_new_members(pop)
+        run_hsp_planner(f'/pddl/story')
+        assign_fitness(pop)
+        remove_worst_from_pop(last=10)
+        add_new_members(pop) # TODO with crossover
